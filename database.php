7436adf33487878a1ea5f0cc4d1422be84a523e7
minnie pangilinan <minnie_pangilinan@yahoo.com.ph>
2013-05-07 12:59:43 +0800

    mysqlpassword

--- database.php
+++ database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'lrmds';
+$db['default']['password'] = '';
 $db['default']['database'] = 'testkpi';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
