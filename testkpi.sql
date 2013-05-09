0cdff42974373af8f629866cfdc1fbc0ff24e9d7
Jasper Cacbay <jasper.cacbay@gmail.com>
2013-05-09 12:35:27 +0800

    db structure and data

--- testkpi.sql
+++ testkpi.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2013 at 05:27 AM
+-- Generation Time: May 09, 2013 at 06:32 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -34,7 +34,55 @@ CREATE TABLE IF NOT EXISTS `fields` (
   PRIMARY KEY (`field_id`),
   KEY `kpi_id` (`kpi_id`),
   KEY `field_id` (`field_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=43 ;
+
+--
+-- Dumping data for table `fields`
+--
+
+INSERT INTO `fields` (`field_id`, `kpi_id`, `field_name`, `type`) VALUES
+(1, 9, 'No. of standard processes per core system across all CU''s', 'int'),
+(2, 10, 'No. of students doing transactions online', 'int'),
+(3, 10, 'No. of processes online', 'int'),
+(4, 11, 'No. of Faculty doing transactions online', 'int'),
+(5, 11, 'No. of Admin staff and REPS doing transactions online', 'int'),
+(6, 11, 'Total alumni transactions done online', 'int'),
+(7, 12, 'Total processing per major transactinos', 'int'),
+(8, 13, 'No. of paper based transactions done online per system', 'int'),
+(9, 14, 'No. of Campuses accessed to online journals per CU', 'int'),
+(10, 15, 'No. of CUs with ICT tools in Research Laboratories', 'int'),
+(11, 15, 'No. of Highschool teachers that integrate IT in their classes', 'int'),
+(12, 15, 'No. of highschools using ICT Tools', 'int'),
+(13, 15, 'No. of ICT equipments in K+12 laboratory schools', 'int'),
+(14, 16, 'No. of transaction volume per admin per system quarterly', 'int'),
+(15, 17, 'No. of faculty using ICT in teaching per CU', 'int'),
+(16, 17, 'No. of faculty using ICT in research per CU', 'int'),
+(17, 18, 'No. of Admin staff using ICT per CU', 'int'),
+(18, 18, 'No. of REPS using ICT per CU', 'int'),
+(19, 19, 'No. of students accessing online learning portals per CU', 'int'),
+(20, 20, 'No. of school services across CU', 'int'),
+(21, 20, 'No. of campuses using shared services (google docs, moodle) per CU', 'int'),
+(22, 21, 'Total no. of standard reports per system', 'int'),
+(23, 21, 'Total no. of Standard Documents (Records and Registries)', 'int'),
+(24, 21, 'Total no. of standard dictionary being used per system', 'int'),
+(25, 21, 'Total no. of standard forms per system. (e.g. for students academics, # of standard change Mat forms', 'int'),
+(26, 22, 'No. of portals using Content Management System (CMS) per CU', 'int'),
+(27, 22, 'No. of available centralized portales per CU', 'int'),
+(28, 23, 'Total no. of donors using online system', 'int'),
+(29, 24, 'Total Expenditures per Cost Center/Year', 'int'),
+(30, 24, 'Annual allocated budget per cost center', 'int'),
+(31, 24, 'Savings per Cost Center per Annum', 'int'),
+(32, 24, 'Income generated per cost centers per annum', 'int'),
+(33, 25, 'Time to generate a specific report requested by higher officials', 'int'),
+(34, 26, 'No. of available dashboards for officials', 'int'),
+(35, 27, 'Total no. of reports available per system', 'int'),
+(36, 27, 'Total no. of data centers across all CUs', 'int'),
+(37, 28, 'Total no. of PCs available to stakeholders per CU', 'int'),
+(38, 28, 'Improvement of Bandwidth to all stakeholders (student, faculty, officials, academic community, and o', 'int'),
+(39, 28, 'No. of servers housed per CU', 'int'),
+(40, 28, 'No. of server rooms per Campus', 'int'),
+(41, 29, 'Clearly defined plantilla of all ICT personell per CU', 'int'),
+(42, 29, 'Clearly defined plantilla of personnel doing ICT work per CU', 'int');
 
 -- --------------------------------------------------------
 
@@ -96,7 +144,42 @@ CREATE TABLE IF NOT EXISTS `kpi` (
   `parent_kpi` int(11) NOT NULL,
   PRIMARY KEY (`kpi_id`),
   KEY `project_id` (`project_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
+
+--
+-- Dumping data for table `kpi`
+--
+
+INSERT INTO `kpi` (`kpi_id`, `project_id`, `kpi_name`, `leaf_node`, `parent_kpi`) VALUES
+(1, 1, 'Simplified Operations', 0, 0),
+(2, 1, 'Improved Efficiency', 0, 0),
+(3, 1, 'Optimal Utilization of ICT Resources', 0, 0),
+(4, 1, 'Increased Productivity', 0, 0),
+(5, 1, 'Extensive Sharing of Information', 0, 0),
+(6, 1, 'Increased Savings/Revenue', 0, 0),
+(7, 1, 'Improved Decision Making Capability', 0, 0),
+(8, 1, 'Improve ICT Infrastructure', 0, 0),
+(9, 1, 'Process Standardization across Constituent Universities', 1, 1),
+(10, 1, 'Student Transactions Done Online', 1, 1),
+(11, 1, 'Faculty, Staff and Alumni Transactions Done Online', 1, 1),
+(12, 1, 'Reduction in transaction processing time', 1, 2),
+(13, 1, 'Reduction of paper based transaction', 1, 2),
+(14, 1, 'Access to faculty, staff and students to learning materials', 1, 3),
+(15, 1, 'Utilization of ICT tools in laboratory schools', 1, 3),
+(16, 1, 'Increase in transaction volume per office', 1, 4),
+(17, 1, 'Faculty members using technology for teaching and research', 1, 4),
+(18, 1, 'Administrative staff and REPS using eUP for greater productivity', 1, 4),
+(19, 1, 'Students using eUP for learning', 1, 4),
+(20, 1, 'Shared services across constituent universities', 1, 5),
+(21, 1, 'Data standardization', 1, 5),
+(22, 1, 'Online portals using content management systems', 1, 5),
+(23, 1, 'Increase in donations, grants, and endowments', 1, 6),
+(24, 1, 'Tracking of cost centers', 1, 6),
+(25, 1, 'Decisions based on timely and accurate data', 1, 7),
+(26, 1, 'Business intelligence software used for decision making', 1, 7),
+(27, 1, 'Single source of truth', 1, 7),
+(28, 1, 'Availability of ICT tools for infrastructure operations', 1, 8),
+(29, 1, 'ICT Manpower consolidation at the level of the constituent universities', 1, 8);
 
 -- --------------------------------------------------------
 
@@ -123,7 +206,14 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `project_id` int(11) NOT NULL AUTO_INCREMENT,
   `project_name` varchar(100) NOT NULL,
   PRIMARY KEY (`project_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`project_id`, `project_name`) VALUES
+(1, 'eup kpi');
 
 -- --------------------------------------------------------
 
